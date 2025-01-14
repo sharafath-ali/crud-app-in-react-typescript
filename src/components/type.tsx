897fe715f238338fc2d613c95f7a53bb2b131d8a
sharafath ali <sharafathaliofficial@gmail.com>
2025-01-14 14:30:00 +0530

    commented unwanted

--- src/components/type.tsx
+++ src/components/type.tsx
@@ -1,56 +1,56 @@
-let union: string | number = "Hello";
+// let union: string | number = "Hello";
 
-let boolean: boolean = true;
-let string: string = "Hello";
-let number: number = 10;
-let role: [number, string] = [1, "admin"];
-let array: Array<string> = ["Hello", "World"];
-let object: { name: string, age: number } = { name: "John", age: 30 };
+// let boolean: boolean = true;
+// let string: string = "Hello";
+// let number: number = 10;
+// let role: [number, string] = [1, "admin"];
+// let array: Array<string> = ["Hello", "World"];
+// let object: { name: string, age: number } = { name: "John", age: 30 };
 
-type Object2way = { name: string; age: number };
-let object2way: Object2way = { name: "John", age: 30 };
+// type Object2way = { name: string; age: number };
+// let object2way: Object2way = { name: "John", age: 30 };
 
-type Object3way = { name: string; age?: number };
-let object3way: Object3way = { name: "John" };
+// type Object3way = { name: string; age?: number };
+// let object3way: Object3way = { name: "John" };
 
-let array2: Object3way[] = [{ name: "John" }, { name: "Jane" }];
+// let array2: Object3way[] = [{ name: "John" }, { name: "Jane" }];
 
-function printName(name: string): string {
-  console.log("Hello " + name);
-  return name
-}
+// function printName(name: string): string {
+//   console.log("Hello " + name);
+//   return name
+// }
 
-printName("John");
-
-
-// y type
-
-type State = {
-  count: number;
-}
+// printName("John");
 
 
-type Propsw = State & {
-  number: number;
-}
+// // y type
 
-let a: Propsw = {
-  count: 0,
-  number: 1
-}
+// type State = {
+//   count: number;
+// }
 
-//inferface
 
-interface State2 {
-  count: number;
-}
+// type Propsw = State & {
+//   number: number;
+// }
 
-interface Statesss extends State2{
-  number: number
-}
+// let a: Propsw = {
+//   count: 0,
+//   number: 1
+// }
 
-let isss : Statesss ={
-  number:5,
-  count:6
-}
+// //inferface
+
+// interface State2 {
+//   count: number;
+// }
+
+// interface Statesss extends State2{
+//   number: number
+// }
+
+// let isss : Statesss ={
+//   number:5,
+//   count:6
+// }
 
